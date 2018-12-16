5ec2a4df2387bbba04169667cabac86cf29209dd
Kevin Woo <kevinawoo@gmail.com>
2018-12-15 22:39:59 -0800

    fix(build): make it work for build systems that run as root

--- bin/build.sh
+++ bin/build.sh
@@ -7,20 +7,22 @@ source bin/env
 rm -rf build/
 
 # builds a debian to build/distributions/*.deb
-docker run --rm \
+# runs as root in the container for some build systems
+docker run -u root --rm \
   -e BUILD_NUMBER="${BUILD_NUMBER}" \
   -e BRANCH_NAME="${BRANCH_NAME}" \
-  -v $(pwd)/.gradle-cache:/home/gradle/.gradle/ \
+  -v $(pwd)/.gradle-cache:/root/.gradle/ \
   -v $(pwd)/:/home/gradle/ \
   gradle:4.10-jre11-slim \
   gradle --no-daemon buildDeb
 
 
 # builds an rpm to build/distributions/*.rpm
-docker run --rm \
+# runs as root in the container for some build systems
+docker run -u root --rm \
   -e BUILD_NUMBER="${BUILD_NUMBER}" \
   -e BRANCH_NAME="${BRANCH_NAME}" \
-  -v $(pwd)/.gradle-cache:/home/gradle/.gradle/ \
+  -v $(pwd)/.gradle-cache:/root/.gradle/ \
   -v $(pwd)/:/home/gradle/ \
   gradle:4.10-jre11-slim \
   gradle --no-daemon buildRpm
